cca8ac601240424abbb8847150bd5ae5dc67296d
Charles Peden and Dan Lauby <dan@>
2017-01-23 09:31:31 -0800

    complete business logic, create first part of front end logic, rename classes & ids in styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,51 @@
-//business logic
-function Contact(first, last) {
-  this.firstName = first;
-  this.lastName = last;
+function Location(place, landmark, timeOfYear, notes) {
+  this.place = place;
+  this.landmark = landmark;
+  this.timeOfYear = timeOfYear;
+  this.notes = notes;
 }
 
-// user interface logic
-$(document).ready(function() {
-  $("form#new-contact").submit(function(event) {
+$(function() {
+  $("form#new-place").submit(function(event){
     event.preventDefault();
 
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedPlace = $("input#place").val();
+    var inputtedLandmark = $("input#landmark").val();
+    var inputtedTimeOfYear = $("input#timeOfYear").val();
+    var inputtedNotes = $("input#notes").val();
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var NewLocation = new Location(inputtedPlace, inputtedLandmark, inputtedTimeOfYear, inputtedNotes);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
-
-    $(".contact").click(function() {
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.firstName);
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-    });
-
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
+    $("ul#places").append("<li><span class='place'>" + newLocation.place + "</span></li>");
   });
 });
+
+//business logic
+// function Contact(first, last) {
+  // this.firstName = first;
+//   this.lastName = last;
+// }
+//
+// // user interface logic
+// $(document).ready(function() {
+//   $("form#new-contact").submit(function(event) {
+//     event.preventDefault();
+//
+//     var inputtedFirstName = $("input#new-first-name").val();
+//     var inputtedLastName = $("input#new-last-name").val();
+//
+//     var newContact = new Contact(inputtedFirstName, inputtedLastName);
+//
+//     $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+//
+//     $(".contact").click(function() {
+//       $("#show-contact").show();
+//       $("#show-contact h2").text(newContact.firstName);
+//       $(".first-name").text(newContact.firstName);
+//       $(".last-name").text(newContact.lastName);
+//     });
+//
+//     $("input#new-first-name").val("");
+//     $("input#new-last-name").val("");
+//   });
+// });
